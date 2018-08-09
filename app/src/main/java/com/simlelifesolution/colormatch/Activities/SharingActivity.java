5a696d31304d66775fb8846584462b334eddc60d
shaon007 <rh.shaon@gmail.com>
2018-08-09 17:27:26 +0100

    Version-1 commit-6

---
+++ app/src/main/java/com/simlelifesolution/colormatch/Activities/SharingActivity.java
@@ -0,0 +1,19 @@
+package com.simlelifesolution.colormatch.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.simlelifesolution.colormatch.R;
+
+public class SharingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sharing);
+
+
+    }
+
+
+}
